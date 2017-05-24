53010ca394cc5e630fb1f9523efcd35b8082e8bf
ggi <ggi@mydebian2>
2017-05-24 13:47:59 +0200

    add body-parser and test function to get params of body for POST request

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-  res.send("Hello Etna's");
+    res.send("Hello Etna's");
 });
 
+app.post('/testpost', function (req, res) {
+    console.log(req.body.test);
+    console.log(req.body.test2)
+    res.json({ message: 'hooray! welcome to our api!' });
+})
+
+
 app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
+    console.log('Example app listening on port 3001!');
 });
+
