4438544485cd6d8fd39d0e1227bd28c18de9dc77
ggi <ggi@mydebian2>
2017-05-24 16:53:34 +0200

    clean bordel + exemple de base pour fonction get et post

--- api/src/server.js
+++ api/src/server.js
@@ -11,10 +11,14 @@ app.post('/testpost', function (req, res) {
     console.log(req.body.test);
     console.log(req.body.test2)
     res.json({ message: 'hooray! welcome to our api!' });
-})
+});
+
+app.get('/crimes/getAllCrimes', function (req, res) {
+    //todo
+    res.json({data: "ici result de mongodb"});
+});
 
 
 app.listen(3001, function () {
     console.log('Example app listening on port 3001!');
 });
-
