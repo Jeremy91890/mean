d0992474dcbd812b190785f366f7494a3a6580cc
Jeremy91890 <jeremy.debelleix@gmail.com>
2017-05-31 15:10:17 +0200

    get crime by id

--- api/src/routes/crimesRoutes.js
+++ api/src/routes/crimesRoutes.js
@@ -1,8 +1,25 @@
 module.exports = function(app) {
     const Crime = require('../models/models').CrimeModel;
 
+    //
+    // GET All crimes
+ 	//
+
     app.get('/crimes/getAllCrimes', function (req, res) {
         Crime.find(function (err, crimes) {
+            if (err)
+                res.json({success: false, message: err});
+            res.json({success: true, crimes: crimes});
+        });
+    });
+
+    //
+    // GET Crime by ID
+    //
+
+    app.get('/crimes/getCrime/:id', function (req, res) {
+    	let id = req.params.id
+        Crime.findOne({_id: id}, function (err, crimes) {
             if (err)
                 res.json({success: false, message: err});
             res.json({success: true, crimes: crimes});
