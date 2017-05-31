9d01eeacb2d1e1a372096be727f5601a22071b94
Galex88 <agerome.alex@gmail.com>
2017-05-31 16:41:20 +0200

    add getAllUsers

--- api/src/routes/usersRoutes.js
+++ api/src/routes/usersRoutes.js
@@ -1,6 +1,12 @@
 module.exports = function(app) {
     const User = require('../models/models').UserModel;
 
-    
+    app.get('/users/getAllUsers', function (req, res) {
+        User.find(function (err, users) {
+            if (err)
+                res.json({success: false, message: err});
+            res.json({success: true, users: users});
+        });
+    });
 
 };
