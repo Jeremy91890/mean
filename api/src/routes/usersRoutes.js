9fa0205503940b6c9a28396e804c7ab6327815a7
Galex88 <agerome.alex@gmail.com>
2017-05-31 17:23:52 +0200

    add functions : addUser, validateUser, deleteUser

--- api/src/routes/usersRoutes.js
+++ api/src/routes/usersRoutes.js
@@ -9,4 +9,37 @@ module.exports = function(app) {
         });
     });
 
+    app.post('/users/addUser', function (req, res) {
+        let newUser = new User();
+
+        newUser.email = req.body.email;
+        newUser.password = req.body.password;
+        newUser.role = req.body.role;
+        newUser.validated = 0;
+
+        newUser.save(function(err){
+            if (err)
+                res.json({success: false, message: err});
+            res.json({success: true, message: "User created"});
+        });
+    });
+
+    app.post('/users/validateUser', function (req, res) {
+        User.findOne({ email: req.body.email }, function (err, userToValidate) {
+            userToValidate.validated = 1;
+            userToValidate.save();
+            if (err)
+                res.json({success: false, message: err});
+        });
+        res.json({success: true, message: "User validated"});
+    });
+
+    app.post('/users/deleteUser', function (req, res) {
+        User.remove({ email: req.body.email }, function (err, userToDel) {
+            if (err)
+                res.json({success: false, message: err});
+        });
+        res.json({success: true, message: "User deleted"});
+    });
+
 };
