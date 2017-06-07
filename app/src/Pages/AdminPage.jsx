3edfff661df319dd008f53b4252d43feef8d7ff2
ggi <ggi@mydebian2>
2017-06-07 11:32:14 +0200

    save validate user

--- app/src/Pages/AdminPage.jsx
+++ app/src/Pages/AdminPage.jsx
@@ -9,6 +9,9 @@ import {
     TableRow,
     TableRowColumn,
 } from 'material-ui/Table';
+import FlatButton from 'material-ui/FlatButton';
+import Snackbar from 'material-ui/Snackbar';
+import {blue500, blue600, red800, white, green800} from 'material-ui/styles/colors';
 import {Container, Row, Col} from 'react-grid-system';
 
 //http utils for request post, get ...
@@ -38,17 +41,26 @@ class AdminPage extends Component {
         this.state = {
             userRole: props.userRole,
             allValidatedUsers: null,
+            allNonValidatedUsers: null,
+
+            showSnackBar: false,
+            messageSnackBar: "",
+            colorSnackBar: red800,
         }
         
         this.processResponseLoadAllValidatedUsers = this.processResponseLoadAllValidatedUsers.bind(this);
+        this.processResponseLoadAllNonValidatedUsers = this.processResponseLoadAllNonValidatedUsers.bind(this);
+
+        this.processResponseValidateUser = this.processResponseValidateUser.bind(this);
     }
 
     componentWillMount() {
         this.loadAllValidatedUsers();
+        this.loadAllNonValidatedUsers();
     }
 
     loadAllValidatedUsers() {
-        var API = API_IP + "/users/getAllUsers/";
+        var API = API_IP + "/users/getAllUsersValidated/";
         var headers = {
             'Accept': 'application/json',
             'Content-Type': 'application/json'};
@@ -56,12 +68,34 @@ class AdminPage extends Component {
     }
 
     processResponseLoadAllValidatedUsers(resp) {
-        console.log(resp)
          if (resp.responseJSON != undefined) {
             resp = resp.responseJSON;
             if (resp.success == true) {
                 this.setState({
                     allValidatedUsers: resp.users
+                });
+            }
+            else {
+                console.log(resp.message)
+            }
+        }     
+    }
+
+    loadAllNonValidatedUsers() {
+        var API = API_IP + "/users/getAllUsersNonValidated/";
+        var headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'};
+        getData(API, headers, this.processResponseLoadAllNonValidatedUsers)
+    }
+
+    processResponseLoadAllNonValidatedUsers(resp) {
+        console.log(resp)
+        if (resp.responseJSON != undefined) {
+            resp = resp.responseJSON;
+            if (resp.success == true) {
+                this.setState({
+                    allNonValidatedUsers: resp.users
                 });
             }
             else {
@@ -76,6 +110,39 @@ class AdminPage extends Component {
 
     processResponseDeleteUser(resp) {
         
+    }
+
+
+    validateUser(user) {
+        var API = API_IP + "/users/validateUser/";
+        var headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'};
+        var data = JSON.stringify({email: user.email});
+        postData(API, headers, data, this.processResponseValidateUser)
+    }
+
+    processResponseValidateUser(resp) {
+        console.log(resp)
+        if (resp.responseJSON != undefined) {
+            resp = resp.responseJSON;
+            if (resp.success == true) {
+                this.setState({
+                    showSnackBar: true,
+                    messageSnackBar: resp.message,
+                    colorSnackBar: green800
+                });
+                this.loadAllValidatedUsers();
+                this.loadAllNonValidatedUsers();
+            }
+            else {
+                this.setState({
+                    showSnackBar: true,
+                    messageSnackBar: resp.message,
+                    colorSnackBar: red800
+                });
+            }
+        }  
     }
 
     render() {
@@ -111,35 +178,48 @@ class AdminPage extends Component {
                             </Table>
                         </div>
                         :
-                        <p>Probleme de chargement</p>
+                        <p>Aucun user</p>
                     }
                     </Col>
                     <Col lg={6}>
-                    Ici user à valider add route get all validated false
-                        <Table>
-                            <TableHeader
-                                displaySelectAll={false}
-                            >
-                                <TableRow>
-                                    <TableHeaderColumn>Email</TableHeaderColumn>
-                                    <TableHeaderColumn>Role</TableHeaderColumn>
-                                    <TableHeaderColumn>Validated</TableHeaderColumn>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody
-                                displayRowCheckbox={false}
-                            >
-                               
+                        {
+                            this.state.allNonValidatedUsers != null
+                            ?
+                            <Table>
+                                <TableHeader
+                                    displaySelectAll={false}
+                                >
                                     <TableRow>
-                                        <TableRowColumn>email</TableRowColumn>
-                                        <TableRowColumn>role</TableRowColumn>
-                                        <TableRowColumn>checkbox</TableRowColumn>
+                                        <TableHeaderColumn>Email</TableHeaderColumn>
+                                        <TableHeaderColumn>Role</TableHeaderColumn>
+                                        <TableHeaderColumn>Action</TableHeaderColumn>
                                     </TableRow>
-                                
-                            </TableBody>
-                        </Table>
+                                </TableHeader>
+                                <TableBody
+                                        displayRowCheckbox={false}
+                                    >
+                                        {this.state.allNonValidatedUsers.map( (row, i) => (
+                                            <TableRow key={i} value={row.email}>
+                                                <TableRowColumn>{row.email}</TableRowColumn>
+                                                <TableRowColumn>{row.role}</TableRowColumn>
+                                                <TableRowColumn><FlatButton label="Valider" onTouchTap={this.validateUser.bind(this, row)}/></TableRowColumn>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                            </Table>
+                            :
+                            <p>Aucun user</p>
+                        }
+                        
                     </Col>
                 </Row>
+                <Snackbar
+                        open={this.state.showSnackBar}
+                        message={this.state.messageSnackBar}
+                        autoHideDuration={4000}
+                        onRequestClose={this.handleRequestCloseSnackBar}
+                        bodyStyle={{textAlign: "center", backgroundColor: this.state.colorSnackBar}}
+                    />
             </div>
         )       
     }
